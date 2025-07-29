04062d1833275f4ef230bb73c94cf442b5bb2139
Carlos <carlos.av.amorim@gmail.com>
2025-07-28 21:08:32 -0300

    Feat: created user services

---
+++ src/main/java/br/com/carlosamorim/taskmanager/service/UserService.java
@@ -0,0 +1,40 @@
+package br.com.carlosamorim.taskmanager.service;
+
+import br.com.carlosamorim.taskmanager.model.User;
+import br.com.carlosamorim.taskmanager.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        return this.userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User save(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public User update(User user) {
+        var userEntity = this.findById(user.getId());
+        BeanUtils.copyProperties(user, userEntity, "id");
+        return this.userRepository.save(userEntity);
+    }
+
+    public void delete(String id) {
+        this.userRepository.deleteById(id);
+    }
+}
